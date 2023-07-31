bd2120f75dd4cf6efc7d737c29b32743896ab914
guilherme <gui-sperandio@hotmail.com>
2023-07-31 18:04:47 -0300

    feat: começando a listagem de plantas

--- app/plants/page.tsx
+++ app/plants/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Plantas | PlantWater",
@@ -7,10 +8,47 @@ export const metadata: Metadata = {
 
 export default function Plants() {
   return (
-    <div className="mt-6 flex items-center ">
-      <div>
-        <h1>oi</h1>
-      </div>
+    <div className="mt-6 p-4 border-2 border-solid border-grayLight rounded-lg">
+      <table className="w-[100%] border-collapse">
+        <tr>
+          <th className="text-left p-2 ">Planta</th>
+          <th className="text-left p-2 ">% Umidade atual</th>
+          <th className="text-left p-2 ">% mínima</th>
+          <th className="text-left p-2 ">% maxima</th>
+        </tr>
+        <tr>
+          <td className="text-left p-2 ">
+            <Link href="/plants/editar/1">Murango</Link>
+          </td>
+          <td className="text-left p-2 ">10 %</td>
+          <td className="text-left p-2 ">12 %</td>
+          <td className="text-left p-2 ">40 %</td>
+        </tr>
+        <tr>
+          <td className="text-left p-2 ">
+            <Link href="/plants/editar/1">Murango</Link>
+          </td>
+          <td className="text-left p-2 ">10 %</td>
+          <td className="text-left p-2 ">12 %</td>
+          <td className="text-left p-2 ">40 %</td>
+        </tr>
+        <tr>
+          <td className="text-left p-2 ">
+            <Link href="/plants/editar/1">Murango</Link>
+          </td>
+          <td className="text-left p-2 ">10 %</td>
+          <td className="text-left p-2 ">12 %</td>
+          <td className="text-left p-2 ">40 %</td>
+        </tr>
+        <tr>
+          <td className="text-left p-2 ">
+            <Link href="/plants/editar/1">Murango</Link>
+          </td>
+          <td className="text-left p-2 ">10 %</td>
+          <td className="text-left p-2 ">12 %</td>
+          <td className="text-left p-2 ">40 %</td>
+        </tr>
+      </table>
     </div>
   );
 }
